81675cdd4b6a7b8f53b5b5094366bdcf23bffeb3
immoonseok <68985393+immoonseok@users.noreply.github.com>
2020-10-30 14:00:48 +0900

    Create web2.sh

---
+++ web2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+sudo apt-get update –y 
+sudo apt-get install apache2 -y 
+
+sudo echo "<html>" > /var/www/html/index.html
+sudo echo "<h1>Server1</h1>" >> /var/www/html/index.html
+sudo echo "</html>" >> /var/www/html/index.html
+sudo echo "<img width="764" src="http://www.dspress.org/news/photo/200809/28_18_425.jpg"></img>" >> /var/www/html/index.html
+
+sudo service apache2 start
+
+
+
+
