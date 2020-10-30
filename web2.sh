51bde61e1cc312ddb83752e9cde99db1622f0be8
immoonseok <68985393+immoonseok@users.noreply.github.com>
2020-10-30 14:02:59 +0900

    Update web2.sh

--- web2.sh
+++ web2.sh
@@ -3,7 +3,7 @@ sudo apt-get update –y
 sudo apt-get install apache2 -y 
 
 sudo echo "<html>" > /var/www/html/index.html
-sudo echo "<h1>Server1</h1>" >> /var/www/html/index.html
+sudo echo "<h1>Server2</h1>" >> /var/www/html/index.html
 sudo echo "</html>" >> /var/www/html/index.html
 sudo echo "<img width="764" src="http://www.dspress.org/news/photo/200809/28_18_425.jpg"></img>" >> /var/www/html/index.html
 
